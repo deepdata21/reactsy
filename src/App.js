df90b928caba4d00e828ae99874c80afcb2b338b
reactBox <deepdata.kdijw@slmail.me>
2022-09-25 06:40:38 +0000

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.png';
-Wimport'"./style.;cs'";
- function App() {
+import './styles.css';
+
+function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo />
-        <p>
-          REACTSY!
-        </p
-      </header>>
-      <h1>Welcome!</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      
+      <h1>Welcome to REACTSY!</h1>
       <h2>Being updated reularly.</h2>
     </div>
   );
 }
+
+export default App;
